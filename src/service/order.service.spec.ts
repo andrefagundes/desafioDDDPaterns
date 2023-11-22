c1f52dbdc5ec7a274875da28aafd61ac8491c41e
André Maciel Fagundes <amfcom@gmail.com>
2023-11-22 11:32:29 -0300

    feat: acrescentando testes em customer e order

--- src/service/order.service.spec.ts
+++ src/service/order.service.spec.ts
@@ -1,3 +1,4 @@
+import Customer from '../entity/customer'
 import Order from '../entity/order'
 import OrderItem from '../entity/order_item'
 import OrderService from './order.service'
@@ -11,5 +12,14 @@ describe('Order service unit tests', () => {
     const order2 = new Order('o2', 'c1', [item2])
     const total = OrderService.total([order, order2])
     expect(total).toBe(500)
+  })
+
+  it('should place an order', () => {
+    const customer = new Customer('1', 'Customer 1')
+    const item1 = new OrderItem('i1', 'OrderItem 1', 10, 1, 'p1')
+
+    const order = OrderService.placeOrder(customer, [item1])
+    expect(customer.rewardPoints).toBe(5)
+    expect(order.total()).toBe(10)
   })
 })
