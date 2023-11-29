d203924a0ac1bf398a8a87671751cdf02e6f9781
André Maciel Fagundes <amfcom@gmail.com>
2023-11-29 09:12:41 -0300

    feat: reorganização dos agregados em módulos

---
+++ src/domain/customer/event/handler/customer-created1.handler.ts
@@ -0,0 +1,10 @@
+import EventHandlerInterface from '../../../@shared/event/event-handler.interface'
+import CustomerCreated1Event from '../customer-created.event'
+
+export default class CustomerCreated1
+  implements EventHandlerInterface<CustomerCreated1Event>
+{
+  handle(event: CustomerCreated1Event): void {
+    console.log('Esse é o primeiro console.log do evento: CustomerCreated')
+  }
+}
