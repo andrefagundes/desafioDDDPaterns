7b060eff83007163df39dad5595dc5229317c8eb
André Maciel Fagundes <amfcom@gmail.com>
2023-12-15 14:31:17 -0300

    feat: :sparkles: Add usecase de criação de product

---
+++ src/usecase/product/create/create.product.usecase.ts
@@ -0,0 +1,22 @@
+import ProductFactory from '../../../domain/product/factory/product.factory'
+import IProductRepository from '../../../domain/product/repository/product-repository.interface'
+import {
+  InputCreateProductDTO,
+  OutputCreateProductDTO,
+} from './create.product.dto'
+
+export default class CreateProductUseCase {
+  constructor(private readonly productRepository: IProductRepository) {}
+
+  async execute(input: InputCreateProductDTO): Promise<OutputCreateProductDTO> {
+    const product = ProductFactory.create(input.type, input.name, input.price)
+
+    await this.productRepository.create(product)
+
+    return {
+      id: product.id,
+      name: product.name,
+      price: product.price,
+    }
+  }
+}
